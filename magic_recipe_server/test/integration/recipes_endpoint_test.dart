74949cad6a253d3446b99b4b32db241f1059aa35
master-8702 <ibroselman@gmail.com>
2025-06-04 13:00:58 +0300

    feat: add test for the soft delete of recipes

--- magic_recipe_server/test/integration/recipes_endpoint_test.dart
+++ magic_recipe_server/test/integration/recipes_endpoint_test.dart
@@ -3,6 +3,7 @@ import 'package:test/test.dart';
 import 'test_tools/serverpod_test_tools.dart';
 
 import 'package:magic_recipe_server/recipes/recipes_endpoint.dart';
+import 'package:magic_recipe_server/src/generated/recipes/recipe.dart';
 
 /// This is our test file for the RecipesEndpoint.
 void main() {
@@ -26,6 +27,56 @@ void main() {
           .generateRecipe(testSessionBuilder, 'chicken, rice, broccoli');
       expect(recipe.text, 'Mock Recipe');
       expect(capturedPrompt, contains('chicken, rice, broccoli'));
+    });
+
+    // testing soft deletion of recipes
+    test(
+        'when calling getRecipes, all recipes that are not deleted are returned',
+        () async {
+      // create a session
+      final session = testSessionBuilder.build();
+
+      // drop all recipes
+      await Recipe.db.deleteWhere(session, where: (t) => t.id.notEquals(null));
+
+      // create a recipe
+      final firstRecipe = Recipe(
+          author: 'Gemini-2.0-flash',
+          text: 'Mock Recipe 1',
+          date: DateTime.now(),
+          ingredients: 'chicken, rice, broccoli');
+
+      await Recipe.db.insertRow(session, firstRecipe);
+
+      // create a second recipe
+      final secondRecipe = Recipe(
+          author: 'Gemini-2.0-flash',
+          text: 'Mock Recipe 2',
+          date: DateTime.now(),
+          ingredients: 'chicken, rice, broccoli');
+      await Recipe.db.insertRow(session, secondRecipe);
+
+      // get all recipes
+      final recipes = await endpoints.recipes.getRecipes(testSessionBuilder);
+
+      // check that the recipes are returned
+      expect(recipes.length, 2);
+
+      // get the first recipe to get its id
+      final recipeToDelete = await Recipe.db.findFirstRow(
+        session,
+        where: (t) => t.text.equals('Mock Recipe 1'),
+      );
+
+      // delete the first recipe
+      await endpoints.recipes
+          .deleteRecipe(testSessionBuilder, recipeToDelete!.id!);
+
+      // get all recipes
+      final recipes2 = await endpoints.recipes.getRecipes(testSessionBuilder);
+      // check that the recipes are returned
+      expect(recipes2.length, 1);
+      expect(recipes2[0].text, 'Mock Recipe 2');
     });
   });
 }
